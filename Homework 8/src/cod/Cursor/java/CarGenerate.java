8f9911fa822667d1e74c8bf9a615821d61de8f2f
Dmytro <65898747+fill19@users.noreply.github.com>
2021-01-12 13:35:25 +0200

    Update CarGenerate.java

--- Homework 8/src/cod/Cursor/java/CarGenerate.java
+++ Homework 8/src/cod/Cursor/java/CarGenerate.java
@@ -9,29 +9,32 @@ public class CarGenerate {
 
 
         public static TheMainCar GeneratorOfCars() {
-        return new TheMainCar(getTradeMark(), getId(),getPrice(), getYear(), getMileage() );
+        return new TheMainCar(getTradeMark(), getId(), getPrice(), getYear(), getMileage());
     }
 
     private static MarkOfCar getTradeMark() {
         int randomizerOfCars = (int) (Math.random() * 5);
         return MarkOfCar.values()[randomizerOfCars];
     }
+
     private static UUID getId() {
         return UUID.randomUUID();
     }
+
     private static int getYear() {
         int min = 2000;
         int max = 2020;
         int random = min + (int) (Math.random() * ((max - min) + 1));
         return random;
     }
+
     private static int getMileage() {
         int min = 10000;
         int max = 99000;
         int interval = max - min;
         Random random = new Random();
         int cars = random.nextInt(interval + 1);
-        return cars ;
+        return cars;
     }
 
     private static int getPrice() {
@@ -40,7 +43,6 @@ public class CarGenerate {
         int price = max - min;
         Random random = new Random();
         int cars = random.nextInt(price + 1);
-        return cars ;
+        return cars;
     }
 }
-
