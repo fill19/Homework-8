2a205d6e615ab2580a763480bbb7868ef7b80340
Dmytro <65898747+fill19@users.noreply.github.com>
2021-01-12 13:33:40 +0200

    Update Solutions.java

--- Homework 8/src/cod/Cursor/java/Solutions.java
+++ Homework 8/src/cod/Cursor/java/Solutions.java
@@ -11,17 +11,17 @@ public class Solutions {
 
         }
         List<TheMainCar> comparisonOfCars = machines.stream()
-                .filter(vehicle -> vehicle.getTradeMark().equals(MarkOfCar.Tesla)  || vehicle.getTradeMark().equals(MarkOfCar.Audi))
+                .filter(vehicle -> vehicle.getTradeMark().equals(MarkOfCar.TESLA) || vehicle.getTradeMark().equals(MarkOfCar.AUDI))
                 .filter(vehicle -> vehicle.getMileage() < 40000)
                 .filter(vehicle -> vehicle.getYear() > 2018)
                 .sorted(Comparator.comparing(TheMainCar::getPrice).reversed())
                 .collect(Collectors.toList());
 
-        System.out.println("One of the chosen cars: " +  "\nyear of the car: " +  "\nmileage of the vehicle: " +   "\nSorted from richest to lower price: " );
+        System.out.println("One of the chosen cars: " + "\nyear of the car: " + "\nmileage of the vehicle: " + "\nSorted from richest to lower price: ");
         System.out.println(comparisonOfCars);
 
-         Map<UUID, TheMainCar> LowestPriceOnCars = comparisonOfCars.stream()
-                .sorted(Comparator.comparing(TheMainCar::getPrice).reversed()).limit(3).collect(Collectors.toMap(TheMainCar::getId,vehicle -> vehicle));
+        Map<UUID, TheMainCar> LowestPriceOnCars = comparisonOfCars.stream()
+                .sorted(Comparator.comparing(TheMainCar::getPrice).reversed()).limit(3).collect(Collectors.toMap(TheMainCar::getId, vehicle -> vehicle));
         System.out.println("\nThe cars with the lowest prices: ");
         LowestPriceOnCars.forEach((key, value) -> System.out.println(key + ", " + value));
     }
